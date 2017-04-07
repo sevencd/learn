8d1e9437cba6a68e5ca6fc86eeef72796d9c4237
Administrator <chen314422@126.com>
2017-04-07 11:04:38 +0800

    添加spring 代理方式

--- src/main/java/designpattern/proxy/Main.java
+++ src/main/java/designpattern/proxy/Main.java
@@ -1,9 +1,24 @@
 package designpattern.proxy;
 
+import org.springframework.aop.framework.ProxyFactory;
+import org.springframework.aop.framework.ProxyFactoryBean;
+
 public class Main {
 	public static void main(String[] args) {
 		IUserDao userDao=new UserDao();
 		IUserDao userDaoProxy=(IUserDao) new MyDynamicHandler().getProxyInstance(userDao);
 		userDaoProxy.save();
+		IUserDao userDaoProxy2=(IUserDao) buildProxyObject(userDao,UserDao.class.getInterfaces());
+		userDaoProxy2.save();
+
+	}
+	public static Object buildProxyObject(Object target, Class<?>[] targetIntf) {
+		Object proxyObject;
+		ProxyFactory pf = null;
+		pf = new ProxyFactory();
+		pf.addInterface(targetIntf[0]);
+		pf.setTarget(target);
+		proxyObject = pf.getProxy();
+		return proxyObject;
 	}
 }
