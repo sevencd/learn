5f50ad232eceeb37a640d57cd4d5e442659d6ed6
sevencd <chen314422@126.com>
2019-10-28 21:22:49 +0800

    二分查找

--- src/main/java/algorithm/search/DichotomizeSearch.java
+++ src/main/java/algorithm/search/DichotomizeSearch.java
@@ -2,36 +2,64 @@ package algorithm.search;
 
 import algorithm.sort.InsertSort;
 
-public class DichotomizeSearch extends Search{
+public class DichotomizeSearch extends Search {
     public DichotomizeSearch(int size) {
-        this.size=size;
-        a=new int[size];
+        this.size = size;
+        a = new int[size];
     }
 
     public static void main(String[] args) {
-        int size=5;
-        DichotomizeSearch search=new DichotomizeSearch(size);
-        search.createData();
+        int size = 5;
+        DichotomizeSearch search = new DichotomizeSearch(size);
+        //search.createData();
+        search.a = new int[]{1, 6, 2, 10, 3};
+        search.size = 5;
         search.display();
-        InsertSort sort=new InsertSort();
-        sort.insertSort(search.a,search.size);
-        int location=search.ordinarySearch(3);
+        InsertSort sort = new InsertSort();
+        int[] result = sort.insertSort(search.a, search.size);
+        //int location=search.ordinarySearch(3);
+        //int location=search.bSearch(result,3);
+        int location = search.bSearch(result, 3, 0, size - 1);
         System.out.print(location);
     }
 
-    private int bSearch(int value) {
+    private int bSearch(int[] a, int value) {
+        int low = 0, high = size - 1;
+        while (low <= high) {
+            int mid = (low + high) / 2;
+            if (a[mid] == value) {
+                return mid;
+            } else if (a[mid] < value) {
+                low = mid + 1;
+            } else {
+                high = mid - 1;
+            }
+        }
         return -1;
     }
+
+    private int bSearch(int[] a, int value, int low, int high) {
+        if (low > high) return -1;
+        int mid = low + ((high - low) >> 1);
+        if (a[mid] == value) {
+            return mid;
+        } else if (a[mid] < value) {
+            return bSearch(a, value, mid + 1, high);
+        } else {
+            return bSearch(a, value, low, mid - 1);
+        }
+    }
+
     private int ordinarySearch(int value) {
-        int location=0;
-        while (location<size){
-            if(value==a[location]){
+        int location = 0;
+        while (location < size) {
+            if (value == a[location]) {
                 return location;
             }
             location++;
         }
-        if(location==size){
-            location=-1;
+        if (location == size) {
+            location = -1;
         }
         return location;
     }
--- src/main/java/algorithm/sort/InsertSort.java
+++ src/main/java/algorithm/sort/InsertSort.java
@@ -5,8 +5,12 @@ package algorithm.sort;
 并保证已排序区间数据一直有序。重复这个过程，直到未排序区间中元素为空，算法结束。
  */
 public class InsertSort extends Sort {
+    public InsertSort() {
+    }
+
     InsertSort(int size) {
         this.size = size;
+
         a = new int[size];
     }
 
@@ -23,6 +27,23 @@ public class InsertSort extends Sort {
     }
 
     // 插入排序，a 表示数组，n 表示数组大小
+    public int[] insertSort(int[] a,int n) {
+        int[] result=new int[a.length];
+        for (int i = 1; i < n; i++) {
+            int value = a[i];
+            //插入点
+            int j;
+            for (j = i; j > 0 && a[j - 1] > value; j--) {
+                //如果当前值小于前面的值,前面的值后移
+                a[j] = a[j - 1];
+            }
+            a[j] = value;
+        }
+        for (int i = 0; i < n; i++) {
+            result[i]=a[i];
+        }
+        return result;
+    }
     public void insertSort() {
         for (int i = 1; i < size; i++) {
             int value = a[i];
